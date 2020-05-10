dc3c0634ee9e317df859da121089d8e157b8b6a1
sugrace <sugrace3000@gmail.com>
2020-05-10 21:00:42 +0900

    complete

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { 
   StyleSheet, 
   Text, 
@@ -7,8 +7,10 @@ import {
   TextInput, 
   Dimensions,
   Platform, 
-  ScrollView
+  ScrollView,
+  AsyncStorage
 } from 'react-native';
+import { AppLoading } from "expo";
 import Todo from './Todo';
 
 
@@ -16,12 +18,85 @@ const { height, width } = Dimensions.get("window");
 
 export default function App() {
   const [newToDo, setNewToDo ] = useState('');
-  
-
+  const [loadedToDos, setLoadedToDos] = useState(false);
+  const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    _loadToDos();
+  },[]);
 
   const _controllNewToDo = useCallback((text) => {
     setNewToDo(text);
   },[])
+ 
+
+  const _loadToDos   = useCallback( async () => {
+    try {
+      const toDos = await AsyncStorage.getItem("toDos");
+      setToDos(JSON.parse(toDos));
+      setLoadedToDos(true);
+    } catch (error) {
+      console.log(error)
+    }
+  },[])
+
+  const _addToDo  = useCallback(() => {
+    if(newToDo !== ''){
+      const ID = Math.floor(Math.random() * 100000 ) ;
+      const newToDoObject = {
+        [ID]:{
+          id: ID,
+          isCompleted: false,
+          text: newToDo,
+          createdAt: Date.now(),
+        }
+      }
+      setToDos((prevState)=>{
+        _saveToDos({...prevState, ...newToDoObject})
+        return {...prevState, ...newToDoObject};
+      })
+    }
+  },[newToDo])
+
+  const _deleteToDo = useCallback((id) => {
+    setToDos((prevState)=>{
+      delete prevState[id];
+      _saveToDos(prevState)
+      return {...prevState};
+    });
+  },[])
+
+  
+  const _updateToDo = useCallback((id, text) => {
+    setToDos((prevState)=>{
+      prevState[id].text = text; 
+      _saveToDos(prevState)
+      return {...prevState};
+    });
+  },[])
+
+  const _saveToDos = useCallback((newToDos) => {
+    const _saveToDos = AsyncStorage.setItem("toDos", JSON.stringify(newToDos));
+  },[])
+
+  const _uncompleteToDo = useCallback((newToDos) => {
+    setToDos((prevState)=>{
+      prevState[id].isCompleted = true; 
+      _saveToDos(prevState)
+      return {...prevState};
+    });
+  },[])
+
+  const _completeToDo = useCallback((newToDos) => {
+    setToDos((prevState)=>{
+      prevState[id].isCompleted = false; 
+      _saveToDos(prevState)
+      return {...prevState};
+    });
+  },[])
+
+  if(!loadedToDos){
+    return <AppLoading/>
+  }
 
 
   return (
@@ -38,9 +113,19 @@ export default function App() {
           placeholderTextColor={"#999"}
           returnKeyType={"done"}
           autoCorrect={false}
+          onSubmitEditing={_addToDo}
         />
         <ScrollView contentContainerStyle={styles.toDos}>
-          <Todo/>
+          {Object.values(toDos).sort((a, b)=> b.createdAt - a.createdAt).map((toDo)=>{
+            return <Todo key={toDo.id}
+                         deleteToDo={_deleteToDo}
+                         uncompleteToDo={_uncompleteToDo}
+                         completeToDo={_completeToDo}
+                         updateToDo={_updateToDo}
+                         {...toDo}
+                    />}
+            )
+          }
         </ScrollView>
       </View>
     </View>
--- Todo.js
+++ Todo.js
@@ -7,34 +7,98 @@ import {
     TextInput, 
     Dimensions,
     Platform, 
-    TouchableOpacity
+    TouchableOpacity,
   } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
 
-export default function Todo(){
+export default function Todo(props){
 const [isEditing, setIsEditing] = useState(false);
-const [isComplete, setIsComplete] = useState(false);
+const [isCompleted, setisCompleted] = useState(false);
+const [toDoValue, setToDoValue] = useState(false);
 
-const _toggleCompelete = useCallback(() => {
-    setIsComplete( prevState => !prevState );
+const _toggleCompelete = useCallback((event) => {
+    event.stopPropagation();
+    setisCompleted( prevState => !prevState );
 },[])
+
+const _startEditing = useCallback((event)=>{
+    event.stopPropagation();
+    setIsEditing(true);
+    setToDoValue(props.text)
+},[])
+
+const _finishEditing = useCallback((event)=>{
+    event.stopPropagation();
+    props.updateToDo(props.id, toDoValue);
+    setIsEditing(false);
+},[toDoValue])
+
+const _controllInput = useCallback((text)=>{
+    setToDoValue(text);
+},[])
+
+
+
 
     return (
         <>
         <View style={styles.container}>
-            <TouchableOpacity onPress={_toggleCompelete}>
-                <View style={[
-                    styles.circle, 
-                    isComplete ? styles.completeCircle : styles.uncompleteCircle
-                    ]}/>
-            </TouchableOpacity>
-            <Text style={styles.text}>hello i am todo</Text>
-        </View>
+            <View style={styles.column}>
+                <TouchableOpacity onPress={_toggleCompelete}>
+                    <View style={[
+                        styles.circle, 
+                        isCompleted ? styles.completeCircle : styles.uncompleteCircle
+                        ]}/>
+                </TouchableOpacity>
+                {isEditing ? (
+                <TextInput style={[
+                            styles.text,
+                            styles.input, 
+                            isCompleted ? styles.completedText : styles.uncompletedText
+                        ]} 
+                        value={toDoValue} 
+                        multiline={true}
+                        onChangeText={_controllInput}
+                />) 
+                : 
+                (
+                    <Text style={[styles.text, isCompleted ? styles.completedText: styles.uncompletedText]}>{props.text}</Text>
+                )}
+                
+            </View>
+                    {isEditing ? (
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={_finishEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>finishEditing</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    ) : (
+                    <View style={styles.actions}>
+                        <TouchableOpacity onPressOut={_startEditing}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>edit</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPressOut={(event)=>{
+                                            event.stopPropagation();
+                                            props.deleteToDo(props.id)
+                                            }}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>x</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    )} 
+            </View>
         </>
     )
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
@@ -43,6 +107,7 @@ const styles = StyleSheet.create({
         borderBottomWidth : StyleSheet.hairlineWidth,
         flexDirection: "row",
         alignItems: "center",
+        justifyContent: "space-between"
     },
     circle:{
         width: 30,
@@ -62,5 +127,29 @@ const styles = StyleSheet.create({
         fontWeight: "600",
         fontSize: 20,
         marginVertical: 20
+    },
+    completedText : {
+        color: "#bbb",
+        textDecorationLine: "line-through"
+    },
+    uncompletedText: {
+        color: "#353839"
+    },
+    column: {
+        flexDirection : "row",
+        alignItems: "center",
+        width: width / 2 ,
+    },
+    actions:{
+        flexDirection: "row"
+    },  
+    actionContainer:{
+        marginVertical: 10,
+        marginHorizontal: 10,
+    },
+    input : {
+        marginVertical: 15,
+        width: width / 2,
+        paddingBottom: 5
     }
 })
